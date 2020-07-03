148ff321296308aa12539c834e6846649995ee48
pretendtrue <pretendtrue@qq.com>
2020-07-03 16:58:24 +0800

    :construction: 工作正在进行中，增加模块切换

--- src/views/customer/edit.vue
+++ src/views/customer/edit.vue
@@ -5,11 +5,25 @@
         <h1>{{ title }}</h1>
       </el-col>
     </el-row>
+    <el-row type="flex" justify="space-between" class="mb-5">
+      <el-col class="colored-cards purple-colored-cards" :class="{ 'filter-none': configure.baseShow}" :span="5" @click.native="switchModular('baseShow')">
+        <i class="el-icon-ice-tea"></i> 客户资料
+      </el-col>
+      <el-col class="colored-cards purplish-red-colored-cards" :class="{ 'filter-none': configure.contactShow}" :span="5" @click.native="switchModular('contactShow')">
+        <i class="el-icon-ice-tea"></i> 联系人信息
+      </el-col>
+      <el-col class="colored-cards blue-colored-cards" :class="{ 'filter-none': configure.bankAccountShow}" :span="5" @click.native="switchModular('bankAccountShow')">
+        <i class="el-icon-ice-tea"></i> 银行卡信息
+      </el-col>
+      <el-col class="colored-cards turquoise-colored-cards" :class="{ 'filter-none': configure.markShow}" :span="5" @click.native="switchModular('markShow')">
+        <i class="el-icon-ice-tea"></i> 唛标
+      </el-col>
+    </el-row>
     <el-row>
-      <el-col><Base /></el-col>
-      <el-col class="mt-8"><Contacts :isEdit="true" /></el-col>
-      <el-col class="mt-8"><BankAccounts :isEdit="true" /></el-col>
-      <el-col class="mt-8"><Mark :isEdit="true" /></el-col>
+      <el-col class="mt-8" v-if="configure.baseShow"><Base /></el-col>
+      <el-col v-if="configure.contactShow"><Contacts :isEdit="true" /></el-col>
+      <el-col v-if="configure.bankAccountShow"><BankAccounts :isEdit="true" /></el-col>
+      <el-col class="mt-8" v-if="configure.markShow"><Mark :isEdit="true" /></el-col>
     </el-row>
   </div>
 </template>
@@ -19,11 +33,18 @@ import Base from "./components/Base";
 import Mark from "./components/Mark";
 import Contacts from "./components/Contacts";
 import BankAccounts from "./components/BankAccounts";
+import { forEach } from 'lodash'
 
 export default {
   data() {
     return {
-      title: "客户管理 - 编辑"
+      title: "客户管理 - 编辑",
+      configure: {
+        baseShow: true,
+        contactShow: false,
+        bankAccountShow: false,
+        markShow: false,
+      }
     };
   },
   components: {
@@ -31,6 +52,19 @@ export default {
     Mark,
     Contacts,
     BankAccounts
+  },
+  methods: {
+    /**
+     * 切换模块
+     */
+    switchModular(type) {
+      forEach(this.configure, (item, key) => {
+        if (item === true) this.configure[key] = false
+      })
+      this.configure[type] = !this.configure[type]
+
+      console.info(this.configure)
+    }
   }
 };
 </script>
--- src/views/manufacturer/create.vue
+++ src/views/manufacturer/create.vue
@@ -5,10 +5,21 @@
         <h1>{{ title }}</h1>
       </el-col>
     </el-row>
+    <el-row type="flex" justify="space-between" class="mb-5">
+      <el-col class="colored-cards purple-colored-cards" :class="{ 'filter-none': configure.baseShow}" :span="5" @click.native="switchModular('baseShow')">
+        <i class="el-icon-ice-tea"></i> 客户资料
+      </el-col>
+      <el-col class="colored-cards purplish-red-colored-cards" :class="{ 'filter-none': configure.contactShow}" :span="5" @click.native="switchModular('contactShow')">
+        <i class="el-icon-ice-tea"></i> 联系人信息
+      </el-col>
+      <el-col class="colored-cards blue-colored-cards" :class="{ 'filter-none': configure.bankAccountShow}" :span="5" @click.native="switchModular('bankAccountShow')">
+        <i class="el-icon-ice-tea"></i> 银行卡信息
+      </el-col>
+    </el-row>
     <el-row>
-      <el-col><Base /></el-col>
-      <el-col class="mt-8"><Contacts :isEdit="false" /></el-col>
-      <el-col class="mt-8"><BankAccounts :isEdit="false" /></el-col>
+      <el-col class="mt-8" v-if="configure.baseShow"><Base /></el-col>
+      <el-col v-if="configure.contactShow"><Contacts :isEdit="false" /></el-col>
+      <el-col v-if="configure.bankAccountShow"><BankAccounts :isEdit="false" /></el-col>
     </el-row>
   </div>
 </template>
@@ -17,17 +28,36 @@
 import Base from "./components/Base";
 import Contacts from "./components/Contacts";
 import BankAccounts from "./components/BankAccounts";
+import { forEach } from 'lodash'
 
 export default {
   data() {
     return {
-      title: "厂家管理 - 添加"
+      title: "厂家管理 - 添加",
+      configure: {
+        baseShow: true,
+        contactShow: false,
+        bankAccountShow: false,
+      }
     };
   },
   components: {
     Base,
     Contacts,
     BankAccounts
+  },
+  methods: {
+    /**
+     * 切换模块
+     */
+    switchModular(type) {
+      forEach(this.configure, (item, key) => {
+        if (item === true) this.configure[key] = false
+      })
+      this.configure[type] = !this.configure[type]
+
+      console.info(this.configure)
+    }
   }
 };
 </script>
