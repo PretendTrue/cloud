38534a7029bb04b4afe86cee658e2acf474160e9
pretendtrue <pretendtrue@qq.com>
2020-08-12 17:16:18 +0800

    部门 CRUD

--- src/api/admin-department.js
+++ src/api/admin-department.js
@@ -4,6 +4,18 @@ export function fetchList(params) {
   return axios.get("/departments", { params });
 }
 
+export function fetchDetails (id) {
+  return axios.get(`/departments/${id}`)
+}
+
+export function store (params) {
+  return axios.post('/departments', params)
+}
+
+export function update (id, params) {
+  return axios.patch(`/departments/${id}`, params)
+}
+
 export function destroy (id) {
   return axios.delete(`/departments/${id}`)
 }
--- src/views/admin/department/components/Base.vue
+++ src/views/admin/department/components/Base.vue
@@ -5,25 +5,13 @@
         <div slot="header" class="clearfix">
           <el-row type="flex" align="middle">
             <el-col :span="12" class="text-left"><span>部门信息</span></el-col>
-            <el-col :span="12" class="text-right"><el-button type="primary">保存</el-button></el-col>
+            <el-col :span="12" class="text-right"><el-button type="primary" @click="onSubmit">保存</el-button></el-col>
           </el-row>
         </div>
         <el-row :gutter="30">
           <el-col :xs="24" :sm="12" :lg="6" :span="4">
             <el-form-item label="部门名称">
               <el-input v-model="form.name"></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :xs="24" :sm="12" :lg="6" :span="4">
-            <el-form-item label="部门权限">
-              <el-select class="w-full" v-model="form.permissions" multiple placeholder="请选择">
-                <el-option
-                  v-for="permission in permissions"
-                  :key="permission.id"
-                  :label="permission.name"
-                  :value="permission.id">
-                </el-option>
-              </el-select>
             </el-form-item>
           </el-col>
         </el-row>
@@ -33,24 +21,53 @@
 </template>
 
 <script>
+import { fetchDetails, store, update } from '@/api/admin-department'
+
 export default {
   data() {
     return {
-      form: {
-        name: '',
-        permissions: []
-      },
-      permissions: [
-        {
-          id: 1,
-          name: '系统管理权限'
-        },
-        {
-          id: 2,
-          name: '仪表盘权限'
-        }
-      ]
+      id: 0,
+      form: {}
     };
+  },
+  created() {
+    let id = this.$route.params && this.$route.params.id
+    if (id > 0) {
+      this.getDetails(id)
+    }
+  },
+  methods : {
+    /**
+     * 详细信息
+     */
+    getDetails (id) {
+      fetchDetails(id).then(response => {
+        this.id = id
+        this.form = response
+      })
+    },
+    /**
+     * 提交表单
+     */
+    onSubmit() {
+      if (this.id > 0) {
+        update(this.id, this.form).then(response => {
+          this.$notify.success({
+            title: '提醒',
+            message: '更新成功'
+          });
+          this.reload();
+        })
+      } else {
+        store(this.form).then(response => {
+          this.$notify.success({
+            title: '提醒',
+            message: '添加成功'
+          });
+          this.$router.push({name: 'admin.department.edit', params: { id: response.id }})
+        })
+      }
+    }
   }
 };
 </script>
